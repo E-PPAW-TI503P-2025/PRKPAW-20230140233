6c1f65fe84423b14e8a75274a0aec0ae8a024a42
MuhammadShidqulIman <muhammadshidqul3@gmail.com>
2025-10-09 22:18:20 +0700

    Tugas CRUD

--- 20230140233-node-server/server.js
+++ 20230140233-node-server/server.js
@@ -1,14 +1,25 @@
  	const express = require('express');
-    const cors = require('cors');
-    const app = express();
-const port = 3000;
+ 	const cors = require('cors'); 
+ 	const app = express();
+ 	const PORT = 3001;
+ 	
+ 	// Middleware
+ 	app.use(cors()); 
+ 	app.use(express.json()); 
+ 	app.use((req, res, next) => {
+ 	  console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
+ 	  next();
+ 	});
+ 	
+ 	app.get('/', (req, res) => {
+ 	  res.send('Home Page for API');
+ 	});
+ 	
+   	
+ 	app.listen(PORT, () => {
+ 	  console.log(`Express server running at http://localhost:${PORT}/`);
+ 	});
+  const bookRoutes = require('./routes/books');
+ 	
+ 	app.use('/api/books', bookRoutes);
 
-app.use(cors());
-
-app.get('/', (req, res) => {
-  res.json({ message: 'Hello from Node.js Server!' });
-});
-
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
